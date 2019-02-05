4469134865cf8ce1458e2fdc814c942485834cc3
sajad1993gh <sajad1993gh@gmail.com>
2019-02-04 20:35:42 -0500

    first

--- 1/throttle.h
+++ 1/throttle.h
@@ -1,4 +1,23 @@
-
+/*
+FILE: throttle.h
+CLASS PROVIDED: throttle (part of the namespace main_savitch_2A)
+CONSTRUCTORS for the throttle class:
+throttle( )
+Postcondition: The throttle has one position above the shut_off position, and it is currently shut off.
+throttle(int size)
+Precondition: size > 0.
+Postcondition: The throttle has size positions above the shut_off position, and it is currently shut off.
+MODIFICATION MEMBER FUNCTIONS for the throttle class:
+void shut_off( )
+Postcondition: The throttle has been turned off.
+void shift(int amount)
+Postcondition: The throttle’s position has been moved by amount (but not below 0 or above the top position).
+CONSTANT MEMBER FUNCTIONS for the throttle class:
+double flow( ) const
+Postcondition: The value returned is the current flow as a proportion of the maximum flow.
+bool is_on( ) const
+Postcondition: If the throttle’s flow is above 0 then the function returns true; otherwise it returns false.
+*/
 class throttle {
 public:
   //Constructor
