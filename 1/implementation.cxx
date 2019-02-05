5699f51a0807adf09b13a4d4226f027cf028d6bf
sajad1993gh <sajad1993gh@gmail.com>
2019-02-04 19:07:48 -0500

    first

--- 1/implementation.cxx
+++ 1/implementation.cxx
@@ -4,3 +4,12 @@ void throttle::shut_off( )
  {
       position = 0;
   }
+
+void throttle::shift(int amount)
+  // Precondition: shut_off has been called at least once to initialize the throttle.
+  // Postcondition: The throttle’s position has been moved by amount (but // not below 0 or above 6).
+  {
+        position += amount;
+  if (position < 0) position = 0;
+  else if (position > 6) position = 6;
+  }
