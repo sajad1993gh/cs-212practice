bbecf5386ca20c5ed91efbaf5189e68b86b2c051
sajad1993gh <sajad1993gh@gmail.com>
2019-04-29 22:56:20 -0400

    more

--- 11/homework.cpp
+++ 11/homework.cpp
@@ -1,16 +1,17 @@
 void search(
 const int a[ ],
-size_t first, size_t size, int target, bool& found, size_t& location
+size_t first, size_t last, int target, bool& found, size_t& location
             )
+// size --> last
 // Precondition: The array segment starting at a[first] and containing size elements is sorted // from smallest to largest.
 // Postcondition: The array segment starting at a[first] and containing size elements has been // searched for the target. If the target was present, then found is true, and location is set so // that target == a[location]. Otherwise, found is set to false.
 // Library facilities used: cstdlib (provides size_t from namespace std)
   {
      size_t middle;
-     if (size == 0) found = false;
+     if (last == 0) found = false;
      else
      {
-       middle = first + size/2;
+       middle = first + (last+1)/2;
         if (target == a[middle])
           {
              location = middle;
@@ -18,9 +19,9 @@ size_t first, size_t size, int target, bool& found, size_t& location
            }
         else if (target < a[middle])
         // The target is less than a[middle], so search before the middle.
-            search(a, first, size/2, target, found, location);
+            search(a, first, last/2, target, found, location);
         else
        // The target must be greater than a[middle], so search after the middle.
-          search(a, middle+1, (size-1)/2, target, found, location);
+            search(a, middle+1, (size-1)/2, target, found, location);
     }
   }
