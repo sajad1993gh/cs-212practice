810ec25bf194a15349a214211b6be1f8e7d738fe
sajad1993gh <sajad1993gh@gmail.com>
2019-05-04 18:16:44 -0400

    more

--- 10/quiz.cpp
+++ 10/quiz.cpp
@@ -1,14 +1,18 @@
-
+template <class Item>
 class heap
 {
  public:
-        ....
+        heap () { used = 0;}
         void push(const Item& entry); // add
         Item& pop(); // remove the highest
+        size_t parent (size_t k) const { return (k-1)/2;}
+        size_t l_child (size_t k) const { return 2*k+1;}
+        size_t r_child (size_t k) const { return 2*k+2;}
  private:
         Item data[CAPACITY];
         size_type used;
 }
+
 
 void push(const Item& entry)
 {
@@ -23,5 +27,25 @@ void push(const Item& entry)
 
 Item& pop()
 {
-
+  if(used == 1)
+  used--;
+  else
+  {
+    data[0] = data[used--];
+    used--;
+    size_t i = 0;
+    while(i != used && data[i] < data[l_child(i)] && data[i] < data[r_child(i)])
+    {
+      if(data[i] < data[l_child(i)])
+      {
+        swap(data[i] , data[l_child(i)]);
+        i = 2*i + 1;
+      }
+      else
+      {
+        swap(data[i] , data[r_child(i)]);
+        i = 2*i + 2;
+      }
+    }
+  }
 }
