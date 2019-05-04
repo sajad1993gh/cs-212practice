830c4d63345da47a2870a210fd05fa8eba036394
sajad1993gh <sajad1993gh@gmail.com>
2019-05-04 18:03:26 -0400

    more

--- 10/quiz.cpp
+++ 10/quiz.cpp
@@ -12,5 +12,16 @@ class heap
 
 void push(const Item& entry)
 {
-  
+  data[used++] = entry;
+  size_t i = used - 1;
+  while(i != 0 && data[i] < data[parent(i)])
+  {
+    swap(data[i], data[parent(i)]);
+    i = (i - 1)/2;
+  }
+}
+
+Item& pop()
+{
+
 }
